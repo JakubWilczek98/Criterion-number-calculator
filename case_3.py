013bb28580595672fa1c53cf67ef1b31efd3ed47
artqxh <artqxh@gmail.com>
2021-12-10 17:42:13 +0100

    mysql configuration

--- case_3.py
+++ case_3.py
@@ -1,3 +1,26 @@
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host='mysql.agh.edu.pl',
+    user='awilcze',
+    password='***** ***',
+    database='awilcze'
+)
+
+mycursor = mydb.cursor()
+
+#mycursor.execute('SHOW TABLES')
+mycursor.execute('SELECT PLACA_POD FROM PRACOWNICY WHERE PLACA_POD = 2666')
+
+myresult = mycursor.fetchall()
+
+oczekiwana = [i[0] for i in myresult][0]
+print(oczekiwana)
+
+print(myresult)
+
+#for x in myresult:
+ #   print(x)
