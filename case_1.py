11a65d081e113e7604730795786bdfd8ae89100a
JakubWilczek98 <jakubwilczek98@gmail.com>
2021-12-16 19:43:57 +0100

    SQL database connection and l_reynoldsa, l_prandtla 2x

--- case_1.py
+++ case_1.py
@@ -1,44 +1,64 @@
 import pandas as pd
 import numpy as np
+import mysql.connector as msql
+from mysql.connector import Error
   
     
-def l_nusselta(dyfuzja_cieplna, wymiar_charakterystyczny, przewodzenie_ciepla):
-    return (dyfuzja_cieplna*wymiar_charakterystyczny)/przewodzenie_ciepla
+def l_nusselta(selected_material, selected_temp, selected_length):
+    
+    return (wspl_dyf_cieplnej*wymiar_charakterystyczny)/wspl_przew_ciepla
 
-def l_reynoldsa(predkosc_plynu, wymiar_charakterystyczny, lepkosc_kinematyczna):
-    return (predkosc_plynu*wymiar_charakterystyczny)/lepkosc_kinematyczna
+def l_reynoldsa(selected_material,selected_temp,selected_velosity, wymiar_charakterystyczny):
+    mycursor.execute("SELECT wspl_lepkosci_dynamicznej FROM {} WHERE temperatura = {}".format(selected_material, selected_temp))
+    myresult = mycursor.fetchall()            
+    wspl_lepkosci_dynamicznej = [i[0] for i in myresult][0] *10e-6
+    return (selected_velosity*wymiar_charakterystyczny)/wspl_lepkosci_dynamicznej
 
-def l_prandtla(lepkosc_kinematyczna, stala_dyfucji_cieplnej):
-    return lepkosc_kinematyczna/stala_dyfucji_cieplnej
+def l_prandtla(selected_material, selected_temp):  
+    mycursor.execute("SELECT wspl_lepkosci_kinematycznej, wspl_dyf_cieplnej FROM {} WHERE temperatura = {}".format(selected_material, selected_temp))
+    myresult = mycursor.fetchall()            
+    wspl_lepkosci_kinematycznej = [i[0] for i in myresult][0]*10e-6
+    wspl_dyf_cieplnej = [i[1] for i in myresult][0]*10e-6
+    return wspl_lepkosci_kinematycznej/wspl_dyf_cieplnej
 
-def l_prandtla_cp(cieplo_wlasciwe,lepkosc_dynamiczna, przewodzenie_ciepla):
-    return (cieplo_wlasciwe*lepkosc_dynamiczna)/przewodzenie_ciepla
-
-
-
-
+def l_prandtla_cp(selected_material, selected_temp):
+    mycursor.execute("SELECT wspl_lepkosci_dynamicznej, cieplo_wlasciwe, wspl_przew_ciepla FROM {} WHERE temperatura = {}".format(selected_material, selected_temp))
+    myresult = mycursor.fetchall()            
+    wspl_lepkosci_dynamicznej = [i[0] for i in myresult][0]*10e-6
+    cieplo_wlasciwe = [i[1] for i in myresult][0]*10e2
+    wspl_przew_ciepla = [i[2] for i in myresult][0]*10e-2
+    return (cieplo_wlasciwe*wspl_lepkosci_dynamicznej)/(wspl_przew_ciepla)
+    
 
 
 if __name__ == "__main__":
     
-    materials = np.array([
-        ["nazwa","woda"],
-        ["gestosc",995.7],
-        ["cieplo wlasciwe", 4187],
-        ["lepkosc kinematyczna", 0.805e-6],
-        ["lepkosc dynamiczna", 801.5e-6 ],
-        ["wspolczynnik przewodzenia ciepla",61.8e-2],
-        ["stala dyfuzji cieplnej", 14.9e-8]
-        ])
+    #Dane wejsciowe
+    selected_temp = 1000
+    selected_material = 'dry_air'
+    wymiar_charakterystyczny = 60e-3
+    selected_velosity = 5
     
-    
-    materials_odczyt = pd.DataFrame(data = materials)
-    
-    print(materials_odczyt)
-    
-    print(l_prandtla_cp(float(materials[2,1]),
-                        float(materials[4,1]),
-                        float(materials[5,1])))
+    try:
+        mydb = msql.connect(host='localhost', 
+                            user='root',
+                            password = '',
+                            database="materials"
+                            )
+        if mydb.is_connected():
+            mycursor = mydb.cursor()
+            print("We are connected to database")  
+            print(l_reynoldsa(selected_material, selected_temp, selected_velosity, wymiar_charakterystyczny))
+            print(l_prandtla(selected_material, selected_temp))
+            print(l_prandtla_cp(selected_material, selected_temp))
+            
+                  
+            
+            
+            
+        
+    except Error as e:
+        print("Connection error", e)
     
     
     
