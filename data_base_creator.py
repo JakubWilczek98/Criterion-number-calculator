350f13ca49bba6ef59e992701409439502d52b8a
artqxh <artqxh@gmail.com>
2021-12-17 11:09:37 +0100

    data base creator fixed

--- data_base_creator.py
+++ data_base_creator.py
@@ -18,7 +18,8 @@ try:
         print("We are connected")
         cursor.execute('DROP TABLE IF EXISTS dry_air;')
         print('Creating table....')
-        cursor.execute("CREATE TABLE dry_air (temperatura INT(3), gestosc FLOAT(4,3), cieplo_wlasciwe FLOAT(4,3), wspl_przew_ciepla FLOAT(3,2), wspl_dyf_cieplnej FLOAT(4,1), wspl_lepkosci_dynamicznej FLOAT(3,1), wspl_lepkosci_kinematycznej FLOAT(6,2), liczba_prandtla FLOAT(4,3))")
+        cursor.execute(
+            "CREATE TABLE dry_air (temperatura INT(3), gestosc FLOAT(4,3), cieplo_wlasciwe FLOAT(4,3), wspl_przew_ciepla FLOAT(3,2), wspl_dyf_cieplnej FLOAT(4,1), wspl_lepkosci_dynamicznej FLOAT(3,1), wspl_lepkosci_kinematycznej FLOAT(6,2), liczba_prandtla FLOAT(4,3))")
         print("dry_air table is created....")
         for i, row in csv_dry_air.iterrows():
             sql = "INSERT INTO awilcze1.dry_air VALUES (%s,%s,%s,%s,%s,%s,%s,%s)"
@@ -28,7 +29,8 @@ try:
             conn.commit()
         cursor.execute('DROP TABLE IF EXISTS water;')
         print('Creating table....')
-        cursor.execute("CREATE TABLE water (temperatura INT(2), cisnienie FLOAT(8,5), gestosc FLOAT(4,1), cieplo_wlasciwe FLOAT(8,3), wspl_przew_ciepla FLOAT(3,1), wspl_dyf_cieplnej FLOAT(3,1), wspl_lepkosci_dynamicznej FLOAT(5,1), wspl_lepkosci_kinematycznej FLOAT(4,3), wspl_rozszerzalnosci FLOAT(3,2) , napiecie_powierzchniowe FLOAT(4,1) ,liczba_prandtla FLOAT(4,2))")
+        cursor.execute(
+            "CREATE TABLE water (temperatura INT(2), cisnienie FLOAT(8,5), gestosc FLOAT(4,1), cieplo_wlasciwe FLOAT(8,3), wspl_przew_ciepla FLOAT(3,1), wspl_dyf_cieplnej FLOAT(6,1), wspl_lepkosci_dynamicznej FLOAT(5,1), wspl_lepkosci_kinematycznej FLOAT(4,3), wspl_rozszerzalnosci FLOAT(3,2) , napiecie_powierzchniowe FLOAT(4,1) ,liczba_prandtla FLOAT(4,2))")
         print("water table is created....")
         for i, row in csv_water.iterrows():
             sql = "INSERT INTO awilcze1.water VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
