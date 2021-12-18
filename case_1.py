820f8f43dd7e6497a23db3b55344a447b70db231
JakubWilczek98 <jakubwilczek98@gmail.com>
2021-12-18 14:59:14 +0100

    Style changes

--- case_1.py
+++ case_1.py
@@ -25,18 +25,18 @@ def l_prandtla_cp(selected_material, selected_temp):
     wspl_przew_ciepla = [i[2] for i in myresult][0]*10**-2
     return (cieplo_wlasciwe*wspl_lepkosci_dynamicznej)/(wspl_przew_ciepla)
     
-def l_grashofa(selected_material, selected_temp, temperatura_wlot, temperatura_sciany):
+def l_grashofa(selected_material, selected_temp, temperatura_sciany):
     g = 9.81
     if selected_material == "dry_air":
         beta = 1/373
     elif selected_material == "water":
-        mycursor.execute("SELECT wspl_rozszerzalnosci FROM {} WHERE temperatura = {}".format(selected_material, abs(temperatura_wlot-temperatura_sciany)))
+        mycursor.execute("SELECT wspl_rozszerzalnosci FROM {} WHERE temperatura = {}".format(selected_material, abs(selected_temp-temperatura_sciany)))
         myresult = mycursor.fetchall()
         beta = [i[0] for i in myresult][0]*10**-4
     mycursor.execute("SELECT wspl_lepkosci_kinematycznej FROM {} WHERE temperatura = {}".format(selected_material, selected_temp))
     myresult = mycursor.fetchall()   
     wspl_lepkosci_kinematycznej = [i[0] for i in myresult][0]*10**-6
-    return (g*wymiar_charakterystyczny**3*beta*abs(temperatura_wlot-temperatura_sciany)/wspl_lepkosci_kinematycznej**2)
+    return (g*wymiar_charakterystyczny**3*beta*abs(selected_temp-temperatura_sciany)/wspl_lepkosci_kinematycznej**2)
 
 
 def rozbieg_hydrauliczny(l_reynoldsa, wymiar_charakterystyczny, dlugosc):
@@ -74,14 +74,14 @@ def rozbieg_hydrauliczny(l_reynoldsa, wymiar_charakterystyczny, dlugosc):
     return eps_L
     
     
-def prf_prw(selected_material, temperatura_wlot, temperatura_sciany):
+def prf_prw(selected_material, selected_temp, temperatura_sciany):
     if selected_material == "dry_air":
-        mycursor.execute("SELECT liczba_prandtla FROM {} WHERE temperatura = {}".format(selected_material, temperatura_wlot))
+        mycursor.execute("SELECT liczba_prandtla FROM {} WHERE temperatura = {}".format(selected_material, selected_temp))
         myresult = mycursor.fetchall()   
         prf = [i[0] for i in myresult][0]
         eps_prf_prw = 1
     elif selected_material == "water":
-        mycursor.execute("SELECT liczba_prandtla FROM {} WHERE temperatura = {}".format(selected_material, temperatura_wlot))
+        mycursor.execute("SELECT liczba_prandtla FROM {} WHERE temperatura = {}".format(selected_material, selected_temp))
         myresult = mycursor.fetchall()   
         prf = [i[0] for i in myresult][0]
         mycursor.execute("SELECT wspl_rozszerzalnosci FROM {} WHERE temperatura = {}".format(selected_material, temperatura_sciany))
@@ -91,7 +91,7 @@ def prf_prw(selected_material, temperatura_wlot, temperatura_sciany):
     return eps_prf_prw, prf
 
 
-def nif_niw(selected_material, temperatura_wlot, temperatura_sciany):
+def etaf_etaw(selected_material, temperatura_wlot, temperatura_sciany):
     mycursor.execute("SELECT gestosc, wspl_lepkosci_kinematycznej FROM {} WHERE temperatura = {}".format(selected_material, temperatura_wlot))
     myresult = mycursor.fetchall()   
     gestosc_f = [i[0] for i in myresult][0]
@@ -102,21 +102,21 @@ def nif_niw(selected_material, temperatura_wlot, temperatura_sciany):
     gestosc_w = [i[0] for i in myresult][0]
     wspl_lepkosci_kinematycznej_w = [i[1] for i in myresult][0]
     
-    nif = gestosc_f * wspl_lepkosci_kinematycznej_f
-    niw = gestosc_w * wspl_lepkosci_kinematycznej_w
-    return nif, niw
+    etaf = gestosc_f * wspl_lepkosci_kinematycznej_f
+    etaw = gestosc_w * wspl_lepkosci_kinematycznej_w
+    return etaf, etaw
 
 if __name__ == "__main__":
     
     #Dane wejsciowe
-    temperatura_wlot = 155 #zmienna wejsciowa!
-    temperatura_wylot = 265  #zmienna wejsciowa! 
-    temperatura_sciany = 270 #zmienna wejsciowa!
+    temperatura_wlot = 100 #zmienna wejsciowa!
+    temperatura_wylot = 100  #zmienna wejsciowa! 
+    temperatura_sciany = 110 #zmienna wejsciowa!
     selected_temp = (temperatura_wlot+temperatura_wylot)/2 
     selected_material = 'dry_air' #zmienna wejsciowa!
-    selected_velosity = 4 #zmienna wejsciowa!
-    wymiar_charakterystyczny = 9*10**-3 #zmienna wejsciowa!
-    dlugosc = 1.6 #dlugosc    
+    selected_velosity = 0.1 #zmienna wejsciowa!
+    wymiar_charakterystyczny = 60*10**-3 #zmienna wejsciowa!
+    dlugosc = 2.1 #dlugosc    
     
     
     try:
@@ -141,7 +141,7 @@ if __name__ == "__main__":
             
             eps_L = rozbieg_hydrauliczny(l_reynoldsa, wymiar_charakterystyczny, dlugosc)
             
-            l_grashofa = l_grashofa(selected_material, selected_temp, temperatura_wlot, temperatura_sciany)
+            l_grashofa = l_grashofa(selected_material, selected_temp, temperatura_sciany)
             
             
             print("Liczba reynoldsa: ")
@@ -151,7 +151,7 @@ if __name__ == "__main__":
             print("Prf: ")
             print(prf)
             print("eps_L: ")
-            print(prf)
+            print(eps_L)
             l_prandtla = l_prandtla(selected_material, selected_temp)
             print("Liczba prandtla:")
             print(l_prandtla)
@@ -163,7 +163,9 @@ if __name__ == "__main__":
             
                 #-----------------------Laminarny------------------------------ Re<=2300
                  
-                l_nusselta = 0.15*(l_reynoldsa)**0.33*(prf)**0.43*(l_grashofa)**0.1*(eps_prf_prw)*0.25*eps_L
+                l_nusselta = 0.15*(l_reynoldsa)**0.33*((prf)**0.43)*((l_grashofa)**0.1)*((eps_prf_prw)**0.25)*eps_L
+                print("eps_prf_prw: ")
+                print(eps_prf_prw)
                 print('l_nusselta:')
                 print(l_nusselta)
             
@@ -171,8 +173,8 @@ if __name__ == "__main__":
                        
                 #-----------------------Przejsciowy------------------------------
         
-                nif , niw = nif_niw(selected_material, temperatura_wlot, temperatura_sciany)
-                l_nusselta = 0.037*(1+(wymiar_charakterystyczny/dlugosc)**(2/3))*((l_reynoldsa)**0.75-180)*l_prandtla**0.42*(nif/niw)**0.14
+                etaf , etaw = etaf_etaww(selected_material, temperatura_wlot, temperatura_sciany)
+                l_nusselta = 0.037*(1+(wymiar_charakterystyczny/dlugosc)**(2/3))*((l_reynoldsa)**0.75-180)*l_prandtla**0.42*(etaf/etaw)**0.14
                 print('l_nusselta:')
                 print(l_nusselta)
             
